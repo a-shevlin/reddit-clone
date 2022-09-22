0ee54bffd47d9130c0b5feeab979c967ded6603c
Alex Shevlin <alexshevlin1@gmail.com>
2022-09-21 17:15:45 -0700

    update UserContext to take postId on comment click.
    Co-authored-by: Matt Herbert <Matth5050@users.noreply.github.com>

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Account from './Account';
 //import { useState } from 'react';
 import ForumControl from './ForumController';
-import Header from "./Header"
+import Header from "./Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { UserContext } from "./UserContext";
 import "bootstrap/dist/js/bootstrap.min.js";
@@ -10,12 +10,14 @@ import "bootstrap/dist/js/bootstrap.min.js";
 function App() {
   const [isLogged, setIsLogged] = useState(false);
   const [userName, setUserName] = useState(null);
+  const [postId, setPostId] = useState(null);
   
   return (
     <Router>
-      <UserContext.Provider value={{isLogged, setIsLogged, userName, setUserName}}>
+      <UserContext.Provider value={{isLogged, setIsLogged, userName, setUserName, postId, setPostId}}>
         <Header />
         <Routes>
+        
           <Route path="/account" element={<Account />} />
           <Route path="/" element={<ForumControl />} />
         </Routes>
