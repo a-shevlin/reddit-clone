e08831d45273d7b2413dc59c162d784d491ed8e2
Alex Shevlin <alexshevlin1@gmail.com>
2022-09-22 12:08:45 -0700

    update to include count function.
    Matt Herbert <Matth5050@users.noreply.github.com>

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,12 +1,88 @@
-import React from "react";
 import PropTypes from "prop-types";
+import  { db, auth } from './../firebase.js';
+import React, { useState, useEffect, useContext }from "react";
+import  {collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc, FieldValue, increment } from 'firebase/firestore';
 
 function Comment(props) {
+
+  const formattedDate = new Date(props.date.seconds * 1000).toLocaleTimeString();
+  const [voteCount, setVoteCount] = useState(props.count);
+
+  useEffect(() => {
+    const countRef = doc(db, "comments", props.id);
+
+      updateDoc(countRef, {
+        count: voteCount
+      });
+  
+  }, [voteCount]);
+
+  useEffect(() => {
+
+    setVoteCount(props.count)
+  
+  }, [props.count]);
+
+  const handleUpVotes = () => {
+    setVoteCount(voteCount => voteCount + 1);
+    console.log("clicked upvote");
+  }
+
+  const handleDownVotes = () => {
+    setVoteCount(voteCount => voteCount - 1);
+    console.log("clicked downvote");
+  }
+
+  // function grabPostId(id) {
+  //   setPostId(id);
+  // }
+
+
   return (
     <React.Fragment>
-      <div className="container" onClick={() => props.whenPostClicked(props.id)}>
-        <h4>{props.content}</h4>
-        <h5>By {props.userName} at {props.date}</h5>
+      <div className="container comment" >
+        <div className="row-top">
+          <div className="commentHeader">
+            <h5>Posted by {props.userName} {formattedDate}</h5>
+            <h3>{props.heading}</h3>
+            <p>{props.content}</p>
+          </div>
+          
+        </div>
+        <div className="row-bottom">
+          <div className="postButtons">
+            <div className="upArrowClip hoverClip pButton" onClick={() => handleUpVotes()}>
+            
+              <div className="upArrow arrow"></div>
+            </div>
+            <div className="count">{props.count}</div>
+            <div className="downArrowClip hoverClip pButton" onClick={() => handleDownVotes()}>
+              <div className="downArrow arrow" ></div> 
+              
+            </div>
+            <div className="comments pButton">
+              <i className="icon" id="commentsClip"></i>
+              <span>comments</span>
+            </div>
+            <a href="/">
+              <div className="share pButton">
+                <i className="icon" id="shareClip"></i>
+                <span>share</span>
+              </div>
+            </a>
+            <a href="/">
+              <div className="save pButton">
+                <i className="icon" id="saveClip"></i>
+                <span>save</span>
+              </div>
+            </a>
+            <a>
+              <div className="ellipsis pButton">
+                <span id="pBtnMore">...</span>
+              </div>
+            </a>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   )
@@ -15,8 +91,13 @@ function Comment(props) {
 Comment.propTypes = { 
   userName: PropTypes.string,
   content: PropTypes.string,
-  date: PropTypes.string,
+  date: PropTypes.exact({
+    seconds: PropTypes.number,
+    nanoseconds: PropTypes.number
+  }), 
+  count: PropTypes.number,
   id: PropTypes.string,
+  postId: PropTypes.string,
   whenPostClicked: PropTypes.func
 };
 
