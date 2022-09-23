100ab6ffd5f121afe39004a1726fe1e1f11d70f9
Alex Shevlin <alexshevlin1@gmail.com>
2022-09-22 17:09:23 -0700

    WIP refresh persistence.
    Co-authored-by: Matt Herbert <Matth5050@users.noreply.github.com>

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,37 @@ import React, { useState } from 'react';
 import Account from './Account';
 //import { useState } from 'react';
 import ForumControl from './ForumController';
-import Header from "./Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { UserContext } from "./UserContext";
+import { UserContext, HeaderState } from "./UserContext";
 import "bootstrap/dist/js/bootstrap.min.js";
+import { Navigate } from 'react-router-dom';
+// import { AuthProvider } from "./Auth";
+// import PrivateRoute from "./PrivateRoute";
+
 
 function App() {
   const [isLogged, setIsLogged] = useState(false);
   const [userName, setUserName] = useState(null);
   const [postId, setPostId] = useState(null);
+  const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
+  const [selectedPost, setSelectedPost] = useState(null);
+  const [editing, setEditing] = useState(false);
+  const [commentFormVisible, setCommentFormVisible] = useState(false);
+  const loggedIn = window.localStorage.getItem("isLoggedIn");
   
+
+  const token = localStorage.getItem('token');
+
   return (
     <Router>
+      <HeaderState.Provider value={{formVisibleOnPage, setFormVisibleOnPage, selectedPost, setSelectedPost, editing, setEditing, commentFormVisible, setCommentFormVisible }}>
       <UserContext.Provider value={{isLogged, setIsLogged, userName, setUserName, postId, setPostId}}>
-        <Header />
         <Routes>
-        
           <Route path="/account" element={<Account />} />
-          <Route path="/" element={<ForumControl />} />
+          <Route exact path="/" element={<ForumControl/>} />
         </Routes>
       </UserContext.Provider>
+      </HeaderState.Provider>
     </Router>
   );
 }
