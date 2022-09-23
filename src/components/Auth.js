c940d81415faaa04d6fe6f27901e3fd58c533e18
Alex Shevlin <alexshevlin1@gmail.com>
2022-09-22 17:13:42 -0700

    WIP Auth and PrivateRoute.
    Co-authored-by: Matt Herbert <Matth5050@users.noreply.github.com>

---
+++ src/components/Auth.js
@@ -0,0 +1,36 @@
+
+import React, { useEffect, useState } from "react";
+import app  from './../firebase.js';
+// import "firebase/auth";
+// import { auth } from './../firebase'
+import { getAuth } from "firebase/auth";
+
+
+export const AuthContext = React.createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [pending, setPending] = useState(true);
+  const auth = getAuth();
+
+  useEffect(() => {
+    app.auth().onAuthStateChanged((user) => {
+      setCurrentUser(user)
+      setPending(false)
+    });
+  }, []);
+
+  if(pending){
+    return <>Loading...</>
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        currentUser
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
