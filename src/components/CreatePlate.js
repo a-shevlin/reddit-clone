111b257189d1ac2c74cc719af8a534955dc3882b
Alex Shevlin <alexshevlin1@gmail.com>
2022-09-22 17:08:25 -0700

    update create plate.

--- src/components/CreatePlate.js
+++ src/components/CreatePlate.js
@@ -3,7 +3,16 @@ import React from 'react';
 function CreatePlate() {
   return (
     <div className="col-item" id="create">
-      <h6>Home</h6>
+      <div className="createBanner">
+        <div id="leftPlanet"></div>
+      </div>
+      <div className="mascotClip"></div>
+      <div className="createText">
+        <h6>Home</h6>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+      <button id="createPost">Create Post</button>
+      <button id="createCommunity" disabled>Create Community</button>
     </div>
   )
 }
