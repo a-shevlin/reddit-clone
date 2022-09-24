a1cc1e2ec0b7fee758b4bf127a882d9449db409c
Matth5050 <matthew.f.herbert@gmail.com>
2022-09-24 00:36:19 -0700

    made changes to allow for proper persistence between sessions

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
+import { auth } from "./../firebase.js";
 import Account from './Account';
+import Login from './Login';
+import SignUp from './SignUp';
 import ForumControl from './ForumController';
+import PrivateRoute from './PrivateRoute.js';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { UserContext, HeaderState } from "./UserContext";
 import "bootstrap/dist/js/bootstrap.min.js";
@@ -13,14 +17,28 @@ function App() {
   const [editing, setEditing] = useState(false);
   const [commentFormVisible, setCommentFormVisible] = useState(false);
   
+  const grabObject = window.sessionStorage.getItem(sessionStorage.key(auth.currentUser));
+  const parseObject = JSON.parse(grabObject);
+
+  useEffect(() => {
+    if (grabObject !== null) {
+      setUserName(parseObject.email);
+    } else {
+      setUserName('Hello!');
+    }
+  },[window.sessionStorage])
 
   return (
     <Router>
       <HeaderState.Provider value={{formVisibleOnPage, setFormVisibleOnPage, selectedPost, setSelectedPost, editing, setEditing, commentFormVisible, setCommentFormVisible }}>
       <UserContext.Provider value={{ userName, setUserName, postId, setPostId}}>
         <Routes>
-          <Route path="/account" element={<Account />} />
-          <Route exact path="/" element={<ForumControl/>} />
+          <Route exact path="/" element={<PrivateRoute />} >
+            <Route exact path="/" element={<ForumControl/>} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/account" element={<Account/>}/>
+          <Route path="/signup" element={<SignUp/>}/>
         </Routes>
       </UserContext.Provider>
       </HeaderState.Provider>
